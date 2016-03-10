16046e9b7ce06aa20f1eadbc494d1bd18fbf110d
Heiko Bernloehr <Heiko.Bernloehr@FreeIT.de>
2016-03-10 13:39:09 +0100

    Remove ParamsParser from middleware stack.

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Vipevalservice
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += %W(#{config.root}/lib)
+    config.middleware.delete "ActionDispatch::ParamsParser"
   end
 end
