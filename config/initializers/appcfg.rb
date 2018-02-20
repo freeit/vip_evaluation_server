c89f0d89fe4df1bf41f4b56fbba7da087cb4eead
Heiko Bernloehr <Heiko.Bernloehr@FreeIT.de>
2018-02-20 23:45:15 +0100

    Log message for APP_CONFIG
    Write log message for APP_CONFIG in appcfg.rb initializer.

--- config/initializers/appcfg.rb
+++ config/initializers/appcfg.rb
@@ -1,5 +1,6 @@
 APP_CONFIG = YAML.load(ERB.new(IO.read(Rails.root.join('config', 'appcfg.yml'))).result)[Rails.env]
 
+Rails.logger.info "***** Initialize APP_CONFIG: #{APP_CONFIG}"
 #Type.load_config_data
 #Resource.load_config_data
 
